d352b1b362e274303eebc49f9aac01726422464b
Xabier San Martín Oneka <xabottol@hotmail.com>
2020-02-17 13:17:55 +0100

    Preparaciones iniciales antes de empezar el proyecto.

--- CapaPresentacion/CapaPresentacion/Program.cs
+++ CapaPresentacion/CapaPresentacion/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace CapaPresentacion
 {
     static class Program
     {
+        public static Acceso gestor = new Acceso();
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
@@ -16,7 +18,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmPrincipal());
         }
     }
 }
--- CapaPresentacion/CapaPresentacion/frmPrincipal.Designer.cs
+++ CapaPresentacion/CapaPresentacion/frmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CapaPresentacion
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
