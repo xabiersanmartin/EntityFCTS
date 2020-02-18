3ff730d8449b1b94384a0699915d138cdf76b2f5
Xabier San Martín Oneka <xabottol@hotmail.com>
2020-02-18 18:03:01 +0100

    Empresas por ciclo, consulta

--- CapaPresentacion/CapaDatos/Gestor.cs
+++ CapaPresentacion/CapaDatos/Gestor.cs
@@ -18,14 +18,14 @@ namespace CapaDatos
             try
             {
                 BdFCTsEntities = new BdFCTsEntities();
-    }
+            }
             catch (Exception)
             {
                 mensaje = "No se a podido conectar con la base de datos, contacte con el administrador";
             }
         }
 
-        public List<Ciclo> DevolverCiclos ()
+        public List<Ciclo> DevolverCiclos()
         {
             return BdFCTsEntities.Ciclos.ToList();
         }
@@ -39,7 +39,7 @@ namespace CapaDatos
 
             if (ciclo == null)
             {
-                mensaje =  "No existe el ciclo ";
+                mensaje = "No existe el ciclo ";
                 return new List<Alumno>();
             }
 
@@ -47,6 +47,27 @@ namespace CapaDatos
             return alumnosDelCiclo;
         }
 
+        public List<Empresa> EmpresasPorCiclo(string idCiclo, out string mensaje)
+        {
+            mensaje = "";
+            Ciclo cicloBuscar = new Ciclo(idCiclo);
+
+            Ciclo ciclo = BdFCTsEntities.Ciclos.ToList().Where(com => com.Equals(cicloBuscar)).SingleOrDefault();
+
+            if (ciclo == null)
+            {
+                mensaje = "No existe el ciclo ";
+                return new List<Empresa>();
+
+            }
+
+            List<OfertasFCT> ofertasCiclo = ciclo.OfertasFCTs.ToList();
+
+            List<Empresa> empresaPorciclo = (from emp in ofertasCiclo
+                                             where emp.IdCiclo == ciclo.Id
+                                             select emp.Empresa).OrderBy(o => o).ToList();
+            return empresaPorciclo;
+        }
 
     }
 }
