e19c0e8fb4383c1fa92f10007bb37c26696f9b69
Xabier San Martín Oneka <xabottol@hotmail.com>
2020-02-19 10:57:18 +0100

    Actualizacion de los nuggets

--- CapaPresentacion/CapaPresentacion/frmEliminarAlumnoDeEmpresa.Designer.cs
+++ CapaPresentacion/CapaPresentacion/frmEliminarAlumnoDeEmpresa.Designer.cs
@@ -28,33 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cboCiclos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboAlumnosCiclo = new System.Windows.Forms.ComboBox();
+            this.btnRetirar = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cboCiclos
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(220, 120);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cboCiclos.FormattingEnabled = true;
+            this.cboCiclos.Location = new System.Drawing.Point(336, 141);
+            this.cboCiclos.Name = "cboCiclos";
+            this.cboCiclos.Size = new System.Drawing.Size(172, 21);
+            this.cboCiclos.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ciclo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Alumnos/as del ciclo";
+            // 
+            // cboAlumnosCiclo
+            // 
+            this.cboAlumnosCiclo.FormattingEnabled = true;
+            this.cboAlumnosCiclo.Location = new System.Drawing.Point(416, 212);
+            this.cboAlumnosCiclo.Name = "cboAlumnosCiclo";
+            this.cboAlumnosCiclo.Size = new System.Drawing.Size(172, 21);
+            this.cboAlumnosCiclo.TabIndex = 3;
+            // 
+            // btnRetirar
+            // 
+            this.btnRetirar.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetirar.Location = new System.Drawing.Point(336, 344);
+            this.btnRetirar.Name = "btnRetirar";
+            this.btnRetirar.Size = new System.Drawing.Size(125, 57);
+            this.btnRetirar.TabIndex = 4;
+            this.btnRetirar.Text = "&Retirar Empresa";
+            this.btnRetirar.UseVisualStyleBackColor = true;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.Location = new System.Drawing.Point(524, 344);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(103, 57);
+            this.btnVolver.TabIndex = 5;
+            this.btnVolver.Text = "&Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
             // 
             // frmEliminarAlumnoDeEmpresa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 510);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnRetirar);
+            this.Controls.Add(this.cboAlumnosCiclo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboCiclos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmEliminarAlumnoDeEmpresa";
             this.Text = "Eliminar Alumno De Empresa";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cboCiclos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboAlumnosCiclo;
+        private System.Windows.Forms.Button btnRetirar;
+        private System.Windows.Forms.Button btnVolver;
     }
 }
